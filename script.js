1ead018980c7646e3a7e64a179569aae781349b6
WhatToCook-Chef <mit.fozzek.holnap@gmail.com>
2025-03-24 22:57:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const API_URL = "https://script.google.com/macros/s/AKfycbze92xbC6gv822hdFa63RuhBVhoOH_coOeYFBvpY9rBHYFfVXPQgrDOIYtTSA8V3Vgh/exec";
 
+fetch(API_URL)
+  .then(response => response.json())
+  .then(data => console.log("Válasz a szerverről:", data))
+  .catch(error => console.error("Hiba történt:", error));
+
 async function getFoods() {
     const response = await fetch(`${API_URL}?action=getFoods`);
     const foods = await response.json();
