fdbd3050f273a670d8ea488ec82cffbfd75f14b7
WhatToCook-Chef <mit.fozzek.holnap@gmail.com>
2025-03-24 22:56:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.google.com/macros/s/AKfycb.../exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbze92xbC6gv822hdFa63RuhBVhoOH_coOeYFBvpY9rBHYFfVXPQgrDOIYtTSA8V3Vgh/exec";
 
 async function getFoods() {
     const response = await fetch(`${API_URL}?action=getFoods`);
